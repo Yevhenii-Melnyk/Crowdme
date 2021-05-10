b64ef058395479e9e9da4b4287d65f56cd90c75b
Yevhenii-Melnyk <evgm1991@gmail.com>
2021-05-11 02:24:47 +0300

    10.05.2021

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ jQuery(document).ready(function($) {
   const $a = $(".header__nav a");
   const $arrowUp = $(".arrowUp");
   const $headerNav = $(".header__nav a");
+  const $projectsSliders = $(".projects__cards");
+  const $commentSliders = $(".comment__items");
 
   $burger.on("click", function() {
     $nav.toggle();
@@ -37,4 +39,51 @@ jQuery(document).ready(function($) {
     console.log($(target).offset().top);
     $("html, body").animate({ scrollTop: $(target).offset().top }, 500);
   });
+
+  const newSliderSettings = {
+    slidesToShow: 1,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: "unslick",
+      },
+    ],
+  };
+
+  $projectsSliders.slick(newSliderSettings);
+
+  $(window).on("resize", function() {
+    if (
+      $(this).width() < 992 &&
+      !$projectsSliders.hasClass("slick-initialized")
+    ) {
+      $projectsSliders.slick(newSliderSettings);
+    }
+  });
+
+  const SliderSettings = {
+    slidesToShow: 1,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 890,
+        settings: "unslick",
+      },
+    ],
+  };
+
+  $commentSliders.slick(SliderSettings);
+
+  $(window).on("resize", function() {
+    if (
+      $(this).width() < 890 &&
+      !$commentSliders.hasClass("slick-initialized")
+    ) {
+      $commentSliders.slick(SliderSettings);
+    }
+  });
+
+  $(".slick-prev").text("<");
+  $(".slick-next").text(">");
 });
